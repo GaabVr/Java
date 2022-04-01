9051a3b3fb6f54a562b3b7697fe5ad8ff7927e1e
Gabriel Sampaio <99880756+GaabVr@users.noreply.github.com>
2022-04-01 12:23:22 -0300

    java.1

--- prog.java
+++ prog.java
@@ -3,6 +3,7 @@ public class programa1 {
     public static void main(String[] args) {
         System.out.println("Seja bem vindo ao nosso canal!!");
         System.out.println("Se inscreva!");
+        System.out.println("Se inscreva agora!")
     }
 
 
