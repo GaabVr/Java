14c5ddb565955ac4e75b091491772e1172497317
Gabriel Sampaio <99880756+GaabVr@users.noreply.github.com>
2022-04-01 12:25:06 -0300

    atua.1

--- prog.java
+++ prog.java
@@ -1,9 +1,12 @@
 public class programa1 {
 
     public static void main(String[] args) {
-        System.out.println("Seja bem vindo ao nosso canal!!");
-        System.out.println("Se inscreva!");
+        System.out.println("Seja bem vindo ao nosso canal!!")
+        System.out.println("Se inscreva!")
         System.out.println("Se inscreva agora!")
+        System.out.println("teste")
+
+        
     }
 
 
