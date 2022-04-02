60a9bf65ec6c6e2f8129741e18ce7c563ae4852a
Gabriel Sampaio <99880756+GaabVr@users.noreply.github.com>
2022-04-01 21:18:00 -0300

    numero inf

--- primeiro prog.java
+++
@@ -1,9 +0,0 @@
-public class primeiro prog {
-    public static void main(String[] args) {
-        System.out.println("Ola mundo");
-        System.out.println("Esse é meu primeiro programa!");
-
-        
-    }
-    
-}
